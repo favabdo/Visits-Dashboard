c77d01a1682680503777daed4070610a71c5d60a
favabdo <abdallah666mo@gmail.com>
2026-09-13 11:11:20 +0300

    Force default cursor on Plotly charts and disable dragmode to remove crosshair

--- client/src/theme/chartTheme.js
+++ client/src/theme/chartTheme.js
@@ -45,6 +45,7 @@ export function baseLayout(title) {
       align: 'right',
     },
     hovermode: 'closest',
+    dragmode: false,
     xaxis: {
       gridcolor: tokens.grid,
       linecolor: tokens.line,
